d37e499be69fde13fdaa2bd11a541ed5fef4489c
m_fraund@u.pacific.edu <30523772+MFraund@users.noreply.github.com>
2023-01-10 16:24:56 -0800

    Small function for dtermining if directory contains a stack or a map of data

--- Functions/StackOrMap.m
+++ Functions/StackOrMap.m
@@ -10,17 +10,27 @@ end
 dirContents = dir(dirPath);
 
 hdrcnt = 0;
+ximcnt = 0;
 for j = 1:length(dirContents) %looping through each file name and counting .hdr files
 	if contains(dirContents(j).name,'.hdr')
 		hdrcnt = hdrcnt + 1;
-		dirLabel = 'stack'; % If there's at least one hdr it must be at least a stack
 		
 		if hdrcnt == 2
-			dirLabel = 'map'; %if 2 or more .hdr files are in one dir, it's a map
+			dirLabel = 'map'; %if 2 or more .hdr files are in one dir, it must be a map, so return
 			return
 			
 		end
+	elseif contains(dirContents(j).name,'.xim')
+		ximcnt = ximcnt + 1;
+		if ximcnt <= 10
+			dirLabel = 'map'; % less than 10 images, could still be a map
+		end
 	end
+	
+end
+
+if ximcnt > 10 && hdrcnt == 1 %many images but only one header, this is a stack
+	dirLabel = 'stack'; % If there's at least one hdr it must be at least a stack
 end
 
 if hdrcnt == 0
